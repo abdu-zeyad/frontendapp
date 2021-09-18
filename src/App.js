cbfb8e746cd06dc3d335094b353a4f0a447531d6
Abdelmajed-zeyad <abd.d@live.com>
2021-09-18 19:55:30 +0300

    initial release

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css';
+import Login from './components/login';
+
+function App() {
+
+  const [token, setToken] = useState('');
+
+  const userLogin = (tok) => {
+    setToken(tok);
+  }
+
+  return (
+    <div className="App">
+      <Login userLogin={userLogin} />
+    </div>
+  );
+}
+
+export default App;
